3b0129ed0645a426804fbe3c6d90cf78c6fddc88
vlad-sem-frontend <106449916+vlad-sem-frontend@users.noreply.github.com>
2023-02-13 18:45:43 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+VANTA.NET({
+  el: "#fon",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x2c5048 ,
+  backgroundColor: 0x9080c
+})
